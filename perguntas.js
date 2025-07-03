4e1ef759f7a2d70a6e48c13b3cb25e4dff69e2cb
RenanBarth <renan.barth@escola.pr.gov.br>
2025-07-03 08:13:15 -0300

    escola

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,111 @@ criaCartao(
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Iugoslávia?',
+    'A capital da Iugoslávia é Belgrado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
