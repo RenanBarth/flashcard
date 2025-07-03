5df7de1d52c4fe4092d89101b999319d426d1722
RenanBarth <renan.barth@escola.pr.gov.br>
2025-07-03 08:37:13 -0300

    escola

--- app.js
+++ app.js
@@ -14,5 +14,12 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+    }
+
     container.appendChild(cartao)
 }
